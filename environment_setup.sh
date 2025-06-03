d210fa77d17cef8b7f3c4e8734deca008242b60d
Dang Van Minh <minhdv0201@gmail.com>
2025-06-03 13:18:09 +0700

    update

--- environment_setup.sh
+++ environment_setup.sh
@@ -33,6 +33,14 @@
 
 
 # ---------- modify ---------------
+#!/usr/bin/env bash
+
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+
+bash Miniconda3-latest-Linux-x86_64.sh
+source /root/miniconda3/etc/profile.d/conda.sh
+command -v conda
+
 # This is required to activate conda environment in scripts
 eval "$(conda shell.bash hook)"
 
